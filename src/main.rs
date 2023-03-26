2ab0d54d2d195c59911cebd1ed882b4016d60963
Jan Ole Hüser <j0le@users.noreply.github.com>
2023-03-26 21:24:49 +0200

    Use the win32 function GetCommandlineW
    and print the commandline as a whole and splitted

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,59 @@
 
+use std::{
+    env, 
+    ffi::OsString,
+    io,
+    io::Write,
+    os::windows::ffi::OsStringExt,
+    thread, 
+    time, 
+};
+
+use windows::Win32::System::Environment ;
+
+
+
 fn main() {
 
+    let mut cmdline_vec : Vec<u16> = vec!();
+    unsafe {
+        let cmdline_ptr = Environment::GetCommandLineW();
+        let mut cmdline_ptr = cmdline_ptr.as_ptr();
+        if cmdline_ptr.is_null() {
+            println!("couldn't get commandline");
+            return;
+        }
+        loop {
+            let c:u16 = *cmdline_ptr;
+            if c == 0 { break; }
+            cmdline_vec.push(c);
+            cmdline_ptr = cmdline_ptr.add(1);
+        }
+    }
+
+    let cmdline : OsString = OsStringExt::from_wide(cmdline_vec.as_slice());
+    match cmdline.to_str() {
+        Some(str) => {
+            println!("could convert commandline losslesly.");
+            println!("Die command line sieht wie folgt aus:\n»{}«", str);
+        },
+        None => {
+            let str = cmdline.to_string_lossy();
+            println!("Die command line sieht wie folgt aus: »{}«", str);
+        }
+    };
+
+    let args: Vec<String> = env::args().collect();
+    for n in 0..args.len() {
+        println!("Das {}. Argument ist: »{}«", n, &args[n]);
+    }
+    
+    loop {
+        print!(".");
+        match io::stdout().flush() {
+            Err(..) => {println!("Cannot flush!"); return;},
+            Ok(()) => {},
+        };
+        thread::sleep(time::Duration::from_millis(2000));
+    }
 }
