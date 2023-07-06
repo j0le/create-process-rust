d12408f17138f598e9bc6efe3f264ca6ee81a2df
Jan Ole Hüser <j0le@users.noreply.github.com>
2023-07-07 00:59:36 +0200

    Fix bug in shell scripts
    the variable script_path wasn't set correctly

--- examples.sh
+++ examples.sh
@@ -3,7 +3,7 @@
 set -e
 set -u
 
-script_dir="$(cd dirname "${0}" && pwd)"
+script_dir="$(cd "$(dirname "${0}")" && pwd)"
 
 "${script_dir}/target/debug/create-process-rust.exe" --json --print-args-only hallo welt 'tach moin' \
 	| jq -C '{ "cmdline": .cmdline, "args": [ .args[].arg ]}'
--- jq-filter.sh
+++ jq-filter.sh
@@ -3,6 +3,4 @@
 set -e
 set -u
 
-#script_dir="$(cd dirname "${0}" && pwd)"
-
 jq -C '{ "cmdline": .cmdline, "args": [ .args[].arg ]}'
