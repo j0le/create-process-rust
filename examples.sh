af761b5ebbb6389a95754cf31d619fdb420675b6
Jan Ole Hüser <j0le@users.noreply.github.com>
2023-07-06 22:08:49 +0200

    Add example shell script that uses jq to filter JSON

---
+++ examples.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+set -e
+set -u
+
+script_dir="$(cd dirname "${0}" && pwd)"
+
+"${script_dir}/target/debug/create-process-rust.exe" --json --print-args-only hallo welt 'tach moin' \
+	| jq -C '{ "cmdline": .cmdline, "args": [ .args[].arg ]}'
