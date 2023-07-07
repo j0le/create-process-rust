8dd6855e3527782155fb97602b88bc71bf3f7230
Jan Ole Hüser <j0le@users.noreply.github.com>
2023-07-08 00:41:15 +0200

    Use "--prepend-program" in cmd.exe.sh script

--- cmd.exe.sh
+++ cmd.exe.sh
@@ -8,7 +8,7 @@ script_dir="$(cd "$(dirname "${0}")" && pwd)"
 
 cmd_path_cygwin="$(which cmd.exe)"
 cmd_path_windows="$(cygpath -wa "${cmd_path_cygwin}")"
-cmd_path_quoted='"'"${cmd_path_windows}"'"'
+#cmd_path_quoted='"'"${cmd_path_windows}"'"'
 
 MSYS_NO_PATHCONV=1 "${script_dir}/target/debug/create-process-rust.exe" \
-	--print-args --program "${cmd_path_windows}" --cmd-line-in-arg "${cmd_path_quoted} ${1}"
+	--print-args --program "${cmd_path_windows}" --prepend-program --cmd-line-in-arg "${1}"
