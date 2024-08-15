f55051e9cb5f246b4b899c13972aa9988a14629b
Jan Ole Hüser <j0le@users.noreply.github.com>
2024-08-15 16:47:02 +0200

    Test the option --cmd-line-utf16-base64

--- scripts/test-base64.sh
+++ scripts/test-base64.sh
@@ -3,6 +3,14 @@
 set -e
 set -u
 
+
+encode() {
+	iconv -f UTF-8 -t UTF-16LE | base64 --wrap=0
+}
+decode() {
+	base64 -d | iconv -f UTF-16LE -t UTF-8
+}
+
 script_dir="$(dirname "${0}")"
 
 cmd_path="$(which cmd.exe)"
@@ -10,18 +18,29 @@ win_path="$(cygpath -w -a "${cmd_path}")"
 printf '%s' "${win_path}"
 printf '\n'
 
-printf '%s' "${win_path}" | iconv -f UTF-8 -t UTF-16LE | base64 --wrap=0
+printf '%s' "${win_path}" | encode
 printf '\n'
-base64_path="$(printf '%s' "${win_path}" | iconv -f UTF-8 -t UTF-16LE | base64 --wrap=0)"
+base64_path="$(printf '%s' "${win_path}" | encode)"
 
-printf '%s' "${win_path}" | iconv -f UTF-8 -t UTF-16LE | base64 --wrap=0 | base64 -d | iconv -f UTF-16LE -t UTF-8
+printf '%s' "${win_path}" | encode | decode
 printf '\n'
 
 cpr="${script_dir}/../cpr.exe"
 
-MSYS_NO_PATHCONV=1 "${cpr}" --print-args --dry-run --program-utf16le-base64 "${base64_path}" --prepend-program --cmd-line-in-arg '/c (echo hello)'
+cmdline='/c (echo hello)'
+base64_cmdline="$(printf '%s' "${cmdline}" | encode)"
+printf '%s\n%s\n' "${cmdline}" "${base64_cmdline}"
+
+printf '%s\n' '------------------------'
+
+MSYS_NO_PATHCONV=1 "${cpr}" --print-args --dry-run \
+	--program-utf16le-base64 "${base64_path}" \
+	--prepend-program \
+	--cmd-line-utf16le-base64 "${base64_cmdline}"
 
 printf '%s\n' '------------------------'
 
 # negativ test
-! MSYS_NO_PATHCONV=1 "${cpr}" --print-args --dry-run --program-utf16le-base64 "öäü" --prepend-program --cmd-line-in-arg '/c (echo hello)' # 2>/dev/null
+! MSYS_NO_PATHCONV=1 "${cpr}" --print-args --dry-run \
+	--program-utf16le-base64 "öäü" \
+	--prepend-program --cmd-line-in-arg "${cmdline}" # 2>/dev/null
