6743dda879e195f642fa78bdb860ae19523b1afb
Jan Ole Hüser <j0le@users.noreply.github.com>
2024-08-15 18:59:00 +0200

    Use cpr to split a commandline

--- scripts/test-print-inner-cmdline.sh
+++ scripts/test-print-inner-cmdline.sh
@@ -9,4 +9,18 @@ cpr(){
 	MSYS_NO_PATHCONV=1 "${script_dir}/../target/debug/create-process-rust.exe" "${@}"
 }
 
+
+cpr --json --split-and-print-inner-cmdline --dry-run --program-is-null --cmd-line-in-arg 'prog "hello world"' \
+	2>/dev/null \
+	| jq '.args[].arg' -C \
+	| less -R
+printf '%s\n' '--------------------------'
+
+cpr --json --print-args --split-and-print-inner-cmdline --dry-run --program-is-null --cmd-line-in-arg 'prog "hello world"' \
+	2>/dev/null \
+	| jq . -C \
+	| less -R
+printf '%s\n' '--------------------------'
+
 cpr --split-and-print-inner-cmdline --dry-run --program-is-null --cmd-line-in-arg 'prog "hello world"'
+printf '%s\n' '--------------------------'
