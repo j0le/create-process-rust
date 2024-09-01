136763bdd57022950dac2f103a89b455e2f5b3d4
Jan Ole Hüser <j0le@users.noreply.github.com>
2024-09-01 16:22:43 +0200

    C++: Let Active codepage and OEM codepage be UTF-8
    Add an execuatble that has a manifest, that tells Windows, that the ACP
    and OEM CP should be UTF-8.

--- cpp/build.sh
+++ cpp/build.sh
@@ -3,4 +3,15 @@
 set -e
 set -u
 
-c++ -static --std=c++23 pargs.cpp -o pargs -Wl,--verbose -v
+mt='C:\Program Files (x86)\Windows Kits\10\bin\10.0.22621.0\x64\mt.exe'
+
+if test -e pargs.o;        then rm pargs.o;        fi
+if test -e pargs.exe;      then rm pargs.exe;      fi
+if test -e pargs-utf8.exe; then rm pargs-utf8.exe; fi
+
+c++ -static --std=c++23 pargs.cpp -c -o pargs.o
+c++ -static --std=c++23 pargs.o -o pargs.exe
+c++ -static --std=c++23 pargs.o -o pargs-utf8.exe \
+	-Xlinker --Xlink=/manifest:EMBED \
+	-Xlinker --Xlink=/manifestinput:UTF8Manifest.xml \
+	-v -Wl,--verbose
--- cpp/pargs.cpp
+++ cpp/pargs.cpp
@@ -1,10 +1,14 @@
 #include <print>
+#include <Windows.h>
 
 int main(int argc, char**argv){
+	std::println("ACP:        {:>10d}", GetACP());
+	std::println("OEM CP:     {:>10d}", GetOEMCP());
+	std::println("Console CP: {:>10d}", GetConsoleCP());
 	for(int i = 0; i < argc; ++i) {
 		std::print("arg {}: \"{}\", bytes: ", i, argv[i]);
 		for(char*c = argv[i]; *c != '\0'; ++c){
-			std::print("{:02x} ", static_cast<unsigned char>(*c));
+			std::print("{:02x} ", *c);
 		}
 		std::println("");
 	}
