dd421082f1d7b6be19409a6dfb6a2c9d47a9253d
Jan Ole Hüser <j0le@users.noreply.github.com>
2024-09-01 17:39:58 +0200

    Move build C++-build-artifacts in folder "build.d"

--- cpp/build.sh
+++ cpp/build.sh
@@ -3,15 +3,21 @@
 set -e
 set -u
 
+script_path="$(dirname "${0}")"
+
 mt='C:\Program Files (x86)\Windows Kits\10\bin\10.0.22621.0\x64\mt.exe'
+build_dir="${script_path}/build.d"
 
-if test -e pargs.o;        then rm pargs.o;        fi
-if test -e pargs.exe;      then rm pargs.exe;      fi
-if test -e pargs-utf8.exe; then rm pargs-utf8.exe; fi
+if test -e "${build_dir}"; then
+	rm -r "${build_dir}"
+fi
+mkdir "${build_dir}"
 
-c++ -static --std=c++23 pargs.cpp -c -o pargs.o
+cd "${build_dir}"
+
+c++ -static --std=c++23 "..\pargs.cpp" -c -o pargs.o
 c++ -static --std=c++23 pargs.o -o pargs.exe
 c++ -static --std=c++23 pargs.o -o pargs-utf8.exe \
-	-Xlinker --Xlink=/manifest:EMBED \
-	-Xlinker --Xlink=/manifestinput:UTF8Manifest.xml \
+	-Xlinker --Xlink=-manifest:EMBED \
+	-Xlinker '--Xlink=-manifestinput:..\UTF8Manifest.xml' \
 	-v -Wl,--verbose
